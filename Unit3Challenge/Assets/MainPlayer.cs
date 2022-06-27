c09163c51d8211b6e75df0318adee45c3d126cc7
gwen <yinniethe1st@yahoo.com>
2022-06-26 19:07:26 -0600

    mainplayer

---
+++ Unit3Challenge/Assets/MainPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+ void Start () 
+{
+    Debug.Log("I am alive!");
+}
+}
---
+++ Unit3Challenge/Assets/mainplayer1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainplayer1 : MonoBehaviour
+{
+    public string myName;
+    
+    // Use this for initialization
+    void Start () 
+    {
+        Debug.Log("I am alive and my name is " + myName);
+    }
+}
